65b8ed52a1de77021d5db91f203ce218e1644c3c
JonathanThomas <JonathanThomas@10.129.51.29>
2016-02-11 14:03:24 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 2/src/Lslist.java changed at Thu Feb 11 14:03:23 EST 2016

---
+++ src/Lslist.java
@@ -0,0 +1,38 @@
+
+public class Lslist {
+    public void clear(){
+        
+    }
+    
+    public void addAfter(Object item){
+        
+    }
+    
+    public void addBefore(Object item){
+        
+    }
+    
+    public void remove(){
+        
+    }
+    
+    public boolean prev(){
+        return false;
+        
+    }
+    
+    public boolean next(){
+        return false;
+        
+    }
+    
+    public int length(){
+        return 0;
+        
+    }
+    
+    public Object getValue() throws IllegalStateException{
+        return null;
+        
+    }
+}
