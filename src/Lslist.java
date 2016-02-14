6d5367ea1cfbcbb5af9b9b4aa90613ab7accf3d0
JonathanThomas <JonathanThomas@10.0.0.124>
2016-02-13 23:55:10 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 2/src/Lslist.java changed at Sat Feb 13 23:55:10 EST 2016

--- src/Lslist.java
+++ src/Lslist.java
@@ -1,10 +1,10 @@
 import java.util.LinkedList;
 
 public class Lslist implements Slist {
-    private LinkedList <Object> list;
-    
+    private LinkedList<Object> list;
+
     private Object currentElement;
-    
+
     private Node<Object> head; // Pointer to list header
     private Node<Object> tail; // Pointer to last element
     private Node<Object> curr; // Pointer to current element
@@ -13,25 +13,26 @@ public class Lslist implements Slist {
     public Lslist() {
         clear();
     }
-    Lslist(int size){
+
+    Lslist(int size) {
         this();
     }
-    
-    //list: <a,b,C,d>
+
+    // list: <a,b,C,d>
     public void clear() {
-        curr = tail = new Node <Object> (null); // Create trailer
-        head = new Node <Object> (tail);        // Create header
+        curr = tail = new Node<Object>(null); // Create trailer
+        head = new Node<Object>(tail); // Create header
         listSize = 0;
     }
 
     public void addAfter(Object item) {
-        Node <Object> holder = new Node<Object>(item);
+        Node<Object> holder = new Node<Object>(item);
         curr.next.next = holder.next;
-        curr.next=holder;
+        curr.next = holder;
     }
 
     public void addBefore(Object item) {
-
+        
     }
 
     public void remove() {
@@ -54,10 +55,10 @@ public class Lslist implements Slist {
     }
 
     public Object getValue() throws IllegalStateException {
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             throw new IllegalStateException();
         }
-        else{
+        else {
             return currentElement;
         }
     }
