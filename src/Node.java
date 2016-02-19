47ec17efcdcc22f63eebce98d523a8b9790e94e8
JonathanThomas <JonathanThomas@10.129.51.10>
2016-02-19 16:57:01 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 2/src/Node.java changed at Fri Feb 19 16:57:01 EST 2016

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,25 @@
+/**
+ * Creates nodes for linked lists.
+ * @author JonathanThomas
+ * @version 2/21/2016
+ * @param <T> is type of node
+ */
 public final class Node<T> {
     public final T       value;
     public       Node<T> next;
     
+    /**
+     * Creates Node that holds value passed in
+     * @param _value is value of the node
+     */
     public Node(T _value) {
         this( _value, null );
     }
+    /**
+     * 
+     * @param _value
+     * @param _next
+     */
     public Node(T _value, Node<T> _next) {
         value = _value;
         next  = _next;
